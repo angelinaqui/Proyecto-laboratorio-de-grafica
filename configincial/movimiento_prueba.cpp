879e0ed8f1ebcbf0819d3c2ecc9ef29db133cd24
CaballeroJuanGpo7 <90823554+CaballeroJuanGpo17@users.noreply.github.com>
2025-05-06 21:09:37 -0600

    Update movimiento_prueba.cpp

--- configincial/movimiento_prueba.cpp
+++ configincial/movimiento_prueba.cpp
@@ -470,6 +470,7 @@ int main()
 		////===============================Dibujado del Dragon================================= 
         for (auto& pos : dragonPositions) {// Caballos
 			model = glm::translate(glm::mat4(1.0f), pos);
+		        model = glm::scale(model, glm::vec3(1.3f));
 			model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //Rotacion180°
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
             dragon.Draw(lightingShader);
